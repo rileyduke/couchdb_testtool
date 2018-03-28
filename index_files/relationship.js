41fa5021f3b11cd274a7c26b0d0fb6913f18b97f
rileyduke <rileyduke@gmail.com>
2018-03-28 04:47:57 -0400

    reuse _repo
    reuse _repo functions from nemakiware

--- index_files/relationship.js
+++ index_files/relationship.js
@@ -1,27 +1,40 @@
 //http://localhost:5984/bedroom/_design/files/_view/relationships
+var missing = [];
 $(function(){
 
 	$(".progress").hide();
 	$("#get-button").click(function(){
-		var getRelationshipUrl = 'http://localhost:5984/bedroom/_design/files/_view/relationships';
-		var getDocumentUrl = 'http://localhost:5984/bedroom/_design/files/_view/docs?key=';
+		var getRelationshipUrl = 'http://localhost:5984/bedroom/_design/_repo/_view/relationships';
+		var getDocumentUrl = 'http://localhost:5984/bedroom/_design/_repo/_view/documents?key=';
 
 		$(".progress").show();
 		$.get(getRelationshipUrl, function(data){
 			data.rows.forEach(function(d){
 				//relationships with parentId exists
-				if(d.value.parentId != null){
-					console.log(d);
-					var getUrl = getDocumentUrl + '%22' + '%22';
-					$.get(getUrl,function(docData){
-						console.log(docData);
-					});
+				if(d.value.sourceId != null){
+					var getUrl = getDocumentUrl + '%22' + d.value.sourceId + '%22';
+					$.get(getUrl,function(x){dumpData(x,d)});
+				}
+
+				if(d.value.targetId != null){
+					var getUrl = getDocumentUrl + '%22' + d.value.targetId + '%22';
+					$.get(getUrl,function(x){dumpData(x,d)});
 				}
 			});
 
 			$("#result").text(JSON.stringify(data));
 			$(".progress").hide();
+			console.log(missing);
 		});
 	});
 	
-})
+});
+
+function dumpData(docData,d){
+	if(docData.rows.length > 0){
+		console.log(docData.rows[0].value);
+	}else{
+		//file is missing
+		missing.push(d);
+	}
+}
