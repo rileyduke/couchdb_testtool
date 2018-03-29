54c8a72c8e4bd220f5dad36ca0ee24b0da4599ed
rileyduke <rileyduke@gmail.com>
2018-03-29 01:36:04 -0400

    paginate relationship get

--- index_files/relationship.js
+++ index_files/relationship.js
@@ -5,34 +5,65 @@ $(function(){
 	$(".progress").hide();
 	$("#get-button").click(function(){
 		var getRelationshipUrl = 'http://localhost:5984/bedroom/_design/_repo/_view/relationships';
+		var getRelationshipCountUrl = 'http://localhost:5984/bedroom/_design/files/_view/relationships_count';
 		var getDocumentUrl = 'http://localhost:5984/bedroom/_design/_repo/_view/documents?key=';
 
+		var relationshipCount = 0;
+		var pageSize = 2;
+
+		var getSequence = [];
+
 		$(".progress").show();
-		$.get(getRelationshipUrl, function(data){
-			data.rows.forEach(function(d){
-				//relationships with parentId exists
-				if(d.value.sourceId != null){
-					var getUrl = getDocumentUrl + '%22' + d.value.sourceId + '%22';
-					$.get(getUrl,function(x){dumpData(x,d)});
-				}
+		$(".progress-bar").css("width","0%")
+		$.get(getRelationshipCountUrl)
+		.done(function(count){
+			relationshipCount = count.rows[0].value;
+			for(i=0; i<=relationshipCount; i+=pageSize){
+				$.get(getRelationshipUrl + "?skip=" + i + "&take=" + pageSize)
+				.done(function(data){
+					$(".progress-bar").css("width", (100*(i/relationshipCount)) + "%")
+					data.rows.forEach(function(d){
+						//relationships with sourceId exists
+						if(d.value.sourceId != null){
+							var getUrl = getDocumentUrl + '%22' + d.value.sourceId + '%22';
+							$.get(getUrl,function(x){dumpData(x,d)});
+						}
 
-				if(d.value.targetId != null){
-					var getUrl = getDocumentUrl + '%22' + d.value.targetId + '%22';
-					$.get(getUrl,function(x){dumpData(x,d)});
-				}
-			});
+						//relationship has targetId
+						if(d.value.targetId != null){
+							var getUrl = getDocumentUrl + '%22' + d.value.targetId + '%22';
+							$.get(getUrl,function(x){dumpData(x,d)});
+						}
+					});
+				});
+				
+			}
+		})
+		// $.get(getRelationshipUrl, function(data){
+		// 	data.rows.forEach(function(d){
+		// 		//relationships with parentId exists
+		// 		if(d.value.sourceId != null){
+		// 			var getUrl = getDocumentUrl + '%22' + d.value.sourceId + '%22';
+		// 			$.get(getUrl,function(x){dumpData(x,d)});
+		// 		}
 
-			$("#result").text(JSON.stringify(data));
-			$(".progress").hide();
-			console.log(missing);
-		});
+		// 		if(d.value.targetId != null){
+		// 			var getUrl = getDocumentUrl + '%22' + d.value.targetId + '%22';
+		// 			$.get(getUrl,function(x){dumpData(x,d)});
+		// 		}
+		// 	});
+
+		// 	$("#result").text(JSON.stringify(data));
+		// 	$(".progress").hide();
+		// 	console.log(missing);
+		// });
 	});
 	
 });
 
 function dumpData(docData,d){
 	if(docData.rows.length > 0){
-		console.log(docData.rows[0].value);
+		console.log(d.id, docData.rows[0].value);
 	}else{
 		//file is missing
 		missing.push(d);
