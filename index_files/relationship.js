d909f0d37e2536e94cec36d06381ca266aa1b72b
rileyduke <rileyduke@gmail.com>
2018-03-29 20:14:17 -0400

    add cdn to local

--- index_files/relationship.js
+++ index_files/relationship.js
@@ -39,24 +39,6 @@ $(function(){
 				
 			}
 		})
-		// $.get(getRelationshipUrl, function(data){
-		// 	data.rows.forEach(function(d){
-		// 		//relationships with parentId exists
-		// 		if(d.value.sourceId != null){
-		// 			var getUrl = getDocumentUrl + '%22' + d.value.sourceId + '%22';
-		// 			$.get(getUrl,function(x){dumpData(x,d)});
-		// 		}
-
-		// 		if(d.value.targetId != null){
-		// 			var getUrl = getDocumentUrl + '%22' + d.value.targetId + '%22';
-		// 			$.get(getUrl,function(x){dumpData(x,d)});
-		// 		}
-		// 	});
-
-		// 	$("#result").text(JSON.stringify(data));
-		// 	$(".progress").hide();
-		// 	console.log(missing);
-		// });
 	});
 	
 });
